7caa943471f4c752256a4cbb429a2a6926819dd8
Frensqq <liadovn@yandex.ru>
2026-04-29 18:42:56 +0300

    create BigButton

---
+++ UiKit/src/main/java/com/example/uikit/Button/BigButton.kt
@@ -0,0 +1,45 @@
+package com.example.uikit.Button
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.uikit.UI.Typography
+import com.example.uikit.UI.colorButton
+import org.w3c.dom.Text
+
+@Composable
+fun BigButton(
+    text: String,
+    onClick: () -> Unit,
+    state: Boolean = true
+){
+
+   Button(
+       onClick,
+       modifier = Modifier.fillMaxWidth()
+           .height(56.dp),
+       enabled = state,
+       colors = colorButton(),
+       shape = RoundedCornerShape(10.dp)
+   ) {
+       Text(
+           text = text,
+           style =  Typography().Title3Semibold
+           )
+   }
+}
+
+@Preview
+@Composable
+fun TestButton(){
+    BigButton("Подтвердить",{}, false)
+}
