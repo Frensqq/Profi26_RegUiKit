fda3e94d18c09ba4f5e43237bfe191d8b9288f02
Frensqq <liadovn@yandex.ru>
2026-04-29 18:07:09 +0300

    add dependencies

---
+++ UiKit/src/main/java/com/example/uikit/UI/Color.kt
@@ -0,0 +1,9 @@
+package com.example.uikit.UI
+
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+
+
